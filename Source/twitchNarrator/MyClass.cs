2d65df3c1eed5d81f888acab69b015acb68ea7c7
Breakfasttt <jeaneaut@gmail.com>
2016-09-04 19:21:23 +0200

    Start project

---
+++ Source/twitchNarrator/MyClass.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: breakfast
+ * Date: 25/08/2016
+ * Time: 22:02
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using RimWorld;
+using System.Collections.Generic;
+
+namespace kappaTwitchStoryTeller
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class MyClass
+	{
+	}
+}
