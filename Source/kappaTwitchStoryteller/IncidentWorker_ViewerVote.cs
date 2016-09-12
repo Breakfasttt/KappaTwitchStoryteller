22d3f130d4d9bc886b14d3fa9e8aaa09ca2741c6
Breakfasttt <jeaneaut@gmail.com>
2016-09-12 23:14:57 +0200

    [add] SimpleTimer Thing
    Add a Simple timer thing to manage some callback like vote timer

--- Source/kappaTwitchStoryteller/IncidentWorker_ViewerVote.cs
+++ Source/kappaTwitchStoryteller/IncidentWorker_ViewerVote.cs
@@ -9,19 +9,93 @@
 using System;
 using Verse;
 using RimWorld;
+using System.Collections.Generic;
 
 namespace KappaTwitchStoryteller
 {
+	
 	/// <summary>
 	/// Description of .
 	/// </summary>
+	/// 
+	
+	
 	public class IncidentWorker_ViewerVote : IncidentWorker
 	{
+		private const float voteTime = 45.0f; // 45 sec for voting
+		private const float mintickBetweenVote = 120.0f; // 2 minutes for next Vote
+		
+		private float lastTickVote;
+		private bool voteActiv;
+		private SimpleTimer voteTimer;
+		private TimerTickFunc myFunc;
+			
+		public IncidentWorker_ViewerVote()
+		{
+			Log.Message("Incident worker creation");
+			lastTickVote = 0.0f;
+			voteActiv = false;
+			voteTimer = ThingMaker.MakeThing(ThingDef.Named("VoteTimer")) as SimpleTimer;
+			Find.TickManager.RegisterAllTickabilityFor(voteTimer);
+			myFunc = timerTick;
+		}
+		
+		protected override bool CanFireNowSub()
+		{
+			Log.Message("==================");
+			
+			if (voteActiv)
+			{
+				Log.Message("vote active");
+				return false;
+			}
+			
+			Log.Message("vote not active");
+
+			
+			return true;
+		}
+		
 		public override bool TryExecute(IncidentParms parms)
 		{
-			VoteWindow voteW = new VoteWindow("test", "desc","vote1", "vote2", "vote3");
+			if (voteActiv)
+				return false;
+
+			Log.Message("Start the vote");
+
+			lastTickVote = Find.TickManager.TicksAbs.TicksToSeconds();
+			voteActiv = true;
+			
+			if(voteTimer.add(myFunc))
+				Log.Message("callback timer correctly added");
+			
+			VoteWindow voteW = new VoteWindow("test", "desc", "vote1", "vote2", "vote3");
 			Find.WindowStack.Add(voteW);
+			Log.Message("==================");
 			return true;
+		}
+		
+		
+		void timerTick(int gameTickAbs)
+		{
+			Log.Message("test try execute");
+			
+			Log.Message("test try execute" + Find.TickManager.TicksAbs.TicksToSeconds() + "/"  +lastTickVote  + "/" + mintickBetweenVote);
+			
+			float delta = (Find.TickManager.TicksAbs.TicksToSeconds() - lastTickVote);
+			
+			if (delta >= voteTime) //Stop voting
+			{
+				Log.Message("Stop Vote");
+				//todo
+			}	
+			else if(delta >= mintickBetweenVote) //reactive for next next vote
+			{
+				voteActiv = false;
+				
+				if(voteTimer.remove(myFunc))
+					Log.Message("callback timer correctly removed");
+			}
 		}
 	}
 }
---
+++ Source/kappaTwitchStoryteller/SimpleTimer.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: breakfast
+ * Date: 12/09/2016
+ * Time: 22:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using Verse;
+using RimWorld;
+
+namespace KappaTwitchStoryteller
+{
+	
+	public delegate void TimerTickFunc(int gameTickAbs);
+	
+	/// <summary>
+	/// Description of SimpleTimer.
+	/// </summary>
+	public class SimpleTimer : Thing
+	{
+		
+		List<TimerTickFunc> callbackList;
+		
+		public SimpleTimer()
+		{
+			callbackList = new List<TimerTickFunc>();
+		}
+		
+		public bool add(TimerTickFunc toAdd)
+		{
+			if(callbackList.Contains(toAdd))
+				return false;
+			
+			callbackList.Add(toAdd);
+			return true;
+		}
+		
+		
+		public bool remove(TimerTickFunc toRemove)
+		{
+			if(!callbackList.Contains(toRemove))
+				return false;
+			
+			callbackList.Remove(toRemove);
+			return true;
+		}
+		
+		public override void Tick()
+		{
+			for(int i = 0; i < callbackList.Count; i++)
+			{
+				callbackList[i](Find.TickManager.TicksAbs);
+			}
+		}
+		
+	}
+}
